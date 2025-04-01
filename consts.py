a17dd83e4945d573ca3c1df18e07b92acd5dbf06
Boaz Lavon <boazlavon@gmail.com>
2025-04-01 16:44:29 +0300

    rm model name

--- consts.py
+++ consts.py
@@ -100,11 +100,6 @@ SUPPORTED_DYNAMIC_SIGNALS = (
     DYNAMIC_SIGNAL__BACKWARD,
 )
 
-MODEL_NAME = "deepseek-ai/deepseek-coder-1.3b-base"
-# MODEL_NAME = "meta-llama/Llama-3.2-1B"
-# MODEL_NAME = "google/gemma-3-1b-it"
-# MODEL_NAME = "meta-llama/Llama-3.1-8B"
-
 GAMMAS = (0.0, 0.1, 0.3, 0.5, 0.7, 0.9)
 # GAMMAS = (0.0, 0.9)
 FILENAME_TEMPLATE = "task_id={task_id}_gamma={gamma}.json"
